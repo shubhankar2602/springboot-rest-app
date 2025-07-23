fc2610eb32761485346376d5148b22bee6be4630
Shubhankar <shubhankarkarajkhede0@gmail.com>
2025-07-23 15:04:53 +0530

    chnaged the Application.java

--- src/main/java/com/oracle/Application.java
+++ src/main/java/com/oracle/Application.java
@@ -8,7 +8,7 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-		//This is demo change 
+		//This is demo change done
 	}
 
 }
